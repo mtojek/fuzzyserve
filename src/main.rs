ffd326487997fbd2fa437fe1a3a7fd0e0da9333f
Marcin Tojek <marcin@coder.com>
2026-01-08 00:23:27 +0100

    AppState

--- src/main.rs
+++ src/main.rs
@@ -18,19 +18,22 @@ struct Args {
     addr: String,
 }
 
+#[derive(Clone)]
+struct AppState {
+    media_root: PathBuf,
+}
+
 async fn index_handler() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-async fn download_handler() -> impl Responder {
+async fn download_handler(path: web::Path<String>, data: web::Data<AppState>) -> impl Responder {
     web::Redirect::to("/files/src/main.rs").see_other()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let args = Args::parse();
-
-    let media_root = args.media_root.clone();
 
     println!(
         "Serving files from {} on http://{}:{}",
@@ -39,8 +42,14 @@ async fn main() -> std::io::Result<()> {
         args.port
     );
 
+    let state = AppState {
+        media_root: args.media_root.clone(),
+    };
+
+    let media_root = args.media_root.clone();
     HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(state.clone()))
             .route("/", web::get().to(index_handler))
             .route("/get/{query:.*}", web::get().to(download_handler))
             .service(Files::new("/files", &media_root).show_files_listing())
